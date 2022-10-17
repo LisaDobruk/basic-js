2a69bc6c5ee2e0a32bb811f9830a402668e045fb
LisaDobruk <liskam8525gmail.com>
2022-10-17 17:44:29 +0200

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let str1 = `${str}`;
+  if( typeof options == 'undefined'){
+    return str1;
+  }
+  
+  
+  if(options.hasOwnProperty('addition')){
+    let addition1 = `${options['addition']}`;
+    
+    if(options.hasOwnProperty('additionRepeatTimes') && options.hasOwnProperty('additionSeparator')){
+      let addition2 = ((addition1 + options['additionSeparator']).repeat((options['additionRepeatTimes'])-1)) + addition1;
+      str1 += addition2;
+    }else if(options.hasOwnProperty('additionRepeatTimes') && !options.hasOwnProperty('additionSeparator')){
+      let addition3 = ((addition1 + '|').repeat(options['additionRepeatTimes'] - 1)) + addition1;
+      str1 += addition3 ;
+    }else{
+      str1 += addition1;
+    }
+    }
+
+    if(options.hasOwnProperty('separator') && options.hasOwnProperty('repeatTimes')){
+      return ((str1 + options['separator']).repeat(options['repeatTimes']-1)) + str1;
+    }else if(options.hasOwnProperty('separator') && !options.hasOwnProperty('repeatTimes')){
+      return str1;
+    }
+    if(options.hasOwnProperty('repeatTimes')){
+      return ((str1 + '+').repeat(options['repeatTimes']-1)) + str1;
+    }
+ 
+  
 }
 
 module.exports = {
